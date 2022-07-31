5f5ec9c9f057bf51d2140c464a4e261008286077
lkp25 <lkp25@o2.pl>
2022-07-31 16:40:07 +0200

    finish styling of ArticlePreview

--- src/components/articlePreview/ArticlePreview.tsx
+++ src/components/articlePreview/ArticlePreview.tsx
@@ -1,19 +1,29 @@
 import { FC } from 'react';
 import { Article } from '../../pages/articles/Articles';
+import './ArticlePreview.css';
 
 interface ArticlePreviewProps {
   article: Article;
 }
 const ArticlePreview: FC<ArticlePreviewProps> = ({ article }) => {
   return (
-    <>
-      <h2>{article.title}</h2>
-      <h3>{article.date}</h3>
-      <img loading="lazy" alt={article.title} src={article.image} />
-
-      {/* optional */}
-      <div> preamble</div>
-    </>
+    <div className="article_preview_container">
+      <img
+        className="article_preview_img"
+        loading="lazy"
+        alt={article.title}
+        src={article.image}
+      />
+      <div className="article_preview_texts_container">
+        <h2 className="article_preview_title">{article.title}</h2>
+        <h3 className="article_preview_date">{article.date}</h3>
+        {/* optional preamble, will render on small mobile screens only */}
+        {/* This will not appear/disappear when window is being resized, but its fine here */}
+        {window.matchMedia(`all and (max-width: 576px)`).matches ? null : (
+          <p className="article_preview_preamble">{article.preamble}</p>
+        )}
+      </div>
+    </div>
   );
 };
 
